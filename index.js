55749e8a9a3c58703370911a5757af8335eb2f29
mojustmo-glitch <maureen.ngugi@student.moringaschool.com>
2023-03-31 00:03:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun (x = "roller-skate"){
+    return "This Saturday, I want to " + x + "!"
+  }
+console.log(saturdayFun())
+
+function mondayWork (b = "go to the office"){
+    return "This Monday, I will " + b + "."
+}
+
+function wrapAdjective (sign){
+    const innerFunction = function (a = "special") {
+        return "You are " + sign + a + sign +"!"; 
+      };
+      return innerFunction; 
+}
